202e73b9224e24bb6bbc23648fbfdfec796a4093
Joseph Hallett <J.Hallett@sms.ed.ac.uk>
2014-10-23 15:05:15 +0100

    Reverting overzealous rubocop change

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -1,10 +1,6 @@
 require 'test/unit'
 
 # Add test libraries you want to use here, e.g. mocha
-class Test
-  class Unit
-    class TestCase
-      # Add global extensions to the test case class here
-    end
-  end
+class Test::Unit::TestCase
+  # Add global extensions to the test case class here
 end
