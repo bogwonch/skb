a15282f3d241c98c84aa94a149a34434b8730d08
Joseph Hallett <J.Hallett@sms.ed.ac.uk>
2014-11-26 17:34:37 +0000

    added error handling to certificate metafetcher

--- metafetchers/certificate.rb
+++ metafetchers/certificate.rb
@@ -6,18 +6,22 @@ module MetaFetcher
   # Extracts the certificate used in the APK
   class Certificate < Skb::MetaFetcher
     def execute
-      Zip::ZipFile.open @apk.path do |apk|
-        cert = apk.find_entry 'META-INF/CERT.RSA'
-        unless cert.nil?
-          Dir.mktmpdir do |dir|
-            cert.extract "#{dir}/CERT.RSA"
-            out = `openssl pkcs7 -in '#{dir}/CERT.RSA' -inform DERM -print_certs | openssl x509 -noout -text`
-            @out << out
+      begin
+        Zip::ZipFile.open @apk.path do |apk|
+          cert = apk.find_entry 'META-INF/CERT.RSA'
+          unless cert.nil?
+            Dir.mktmpdir do |dir|
+              cert.extract "#{dir}/CERT.RSA"
+              out = `openssl pkcs7 -in '#{dir}/CERT.RSA' -inform DERM -print_certs | openssl x509 -noout -text`
+              @out << out
+            end
+            return true
           end
-          return true
         end
+        return false
+      rescue => _e
+        return false
       end
-      return false
     end
   end
 end
